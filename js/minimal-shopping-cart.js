e94bd4d27bb2f61cef25d1bc6a6b32673cd5b201
foy4748 <faisaljfcl@gmail.com>
2024-09-15 12:05:22 +0600

    Added minimal shopping cart

---
+++ js/minimal-shopping-cart.js
@@ -0,0 +1,28 @@
+const cardsContainer = document.getElementById("cards-section");
+const cartItemSection = document.getElementById("shopping-cart-section");
+
+const cards = cardsContainer.getElementsByTagName("section");
+
+function singleCartItem(itemName, price) {
+  return `
+        <!-- Single Cart Item Band -->
+        <div class="bg-slate-100 p-2 flex justify-between items-center">
+          <p class="text-sm">${itemName}</p>
+          <p>${price}</p>
+        </div>
+
+	`;
+}
+
+//const singleCard = cards[0];
+for (const singleCard of cards) {
+  const cardBtn = singleCard.querySelector("button");
+
+  cardBtn.addEventListener("click", function () {
+    const itemName = singleCard.querySelector("h1").innerText;
+    const itemPrice = singleCard.querySelector("h2").innerText;
+    const newRowHTML = singleCartItem(itemName, itemPrice);
+    cartItemSection.innerHTML += newRowHTML;
+    cardBtn.setAttribute("disabled", true);
+  });
+}
